c38acca69ca228bb2c48661366d0cffc702af859
Ken Alex Fassone <nexusventuri@gmail.com>
2020-05-18 23:24:58 +0100

    Create hello.c example
    Added basic script to compile and run it

---
+++ README.md
@@ -0,0 +1,8 @@
+# Wasm playground
+
+Yeah, I'd like to see how javascript and c code can share data, especially more complex data structures, pointers and others
+
+# FAQ
+
+- Why are most of your projects called playground?
+yes
---
+++ compile.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v emcc)" ]; then
+  echo 'emcc is not installed, please follow the installation instructions here:' >&2
+  echo 'https://webassembly.org/getting-started/developers-guide/' >&2
+  exit 1
+fi
+
+file_to_compile="$1"
+if [[ ! -f "$file_to_compile" ]]; then
+  echo 'Please pass a filename as an argument, example:'
+  echo "$ $0 file_to_compile.c"
+  exit 1
+fi
+
+if [[ -f 'tmp' ]]; then
+  mkdir tmp
+fi
+
+file_with_no_extention="${file_to_compile%.*}"
+
+emcc "$file_to_compile" -o "tmp/$file_with_no_extention.html"
+
+echo "Running the compiled page, open it on:"
+echo "http://localhost:8080/$file_with_no_extention.html"
+echo ""
+echo ""
+emrun --no_browser --port 8080 tmp/
