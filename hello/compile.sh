528da67449ea68d5ae8ac3c9a6758564a0a237d4
Ken Alex Fassone <nexusventuri@gmail.com>
2020-05-26 00:18:25 +0100

    Update hello example
    Making sure I can build other examples

--- hello/compile.sh
+++ hello/compile.sh
@@ -6,23 +6,20 @@ if ! [ -x "$(command -v emcc)" ]; then
   exit 1
 fi
 
-file_to_compile="$1"
+file_to_compile="hello.c"
 if [[ ! -f "$file_to_compile" ]]; then
   echo 'Please pass a filename as an argument, example:'
   echo "$ $0 file_to_compile.c"
   exit 1
 fi
-
-if [[ -f 'tmp' ]]; then
-  mkdir tmp
+if [[ ! -d 'tmp' ]]; then
+  mkdir 'tmp'
 fi
 
-file_with_no_extention="${file_to_compile%.*}"
-
-emcc "$file_to_compile" -o "tmp/$file_with_no_extention.html"
+emcc "$file_to_compile" -o "tmp/index.html"
 
 echo "Running the compiled page, open it on:"
-echo "http://localhost:8080/$file_with_no_extention.html"
+echo "http://localhost:8080/index.html"
 echo ""
 echo ""
 emrun --no_browser --port 8080 tmp/
